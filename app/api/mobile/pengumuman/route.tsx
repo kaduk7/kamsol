3ff50bbf995d76f473f0525839799933a16c1438
kaduk7 <zulakmal24@gmail.com>
2024-04-16 22:02:27 +0700

    tes

---
+++ app/api/mobile/pengumuman/route.tsx
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export const POST = async (request: Request) => {
+    const body: any = await request.json();
+    const mapData = JSON.parse(body.data);
+    const formData = await request.formData()
+    await prisma.pengumumanTb.create({
+        data: {
+            judul: mapData.judul,
+            isi: mapData.isi,
+        }
+    })
+    return NextResponse.json({ pesan: 'berhasil' })
+}
+
+export const GET = async () => {
+    const user = await prisma.pengumumanTb.findMany({
+        orderBy: {
+            judul: 'asc'
+        }
+    });
+    return NextResponse.json(user, { status: 200 })
+}
