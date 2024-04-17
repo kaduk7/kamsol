c22369c6c1305be0edf04dd0075085da6130bf4f
kaduk7 <zulakmal24@gmail.com>
2024-04-17 11:17:15 +0700

    12121

--- app/api/mobile/pengumuman/route.tsx
+++ app/api/mobile/pengumuman/route.tsx
@@ -6,13 +6,13 @@ const prisma = new PrismaClient()
 
 export const POST = async (request: Request) => {
     const body = await request.json();
-    await prisma.pengumumanTb.create({
+    const pengumuman = await prisma.pengumumanTb.create({
         data: {
             judul: body.judul,
             isi: body.isi,
         }
     })
-    return NextResponse.json({ pesan: 'berhasil' })
+    return NextResponse.json({ pesan: 'berhasil',data: pengumuman })
 }
 
 export const GET = async () => {
